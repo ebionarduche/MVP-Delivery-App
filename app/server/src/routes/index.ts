777127f6d7643c4a0120447a1b94868e8e541b45
Ebio Narduche <ebionarduche77@gmail.com>
2024-01-19 17:31:59 -0300

    feat: Add ProductsType - Models, Service, Controller and Router

--- app/server/src/routes/index.ts
+++ app/server/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import categoriesRouter from './categories.routes';
 import productsRouter from './products.routes';
+import productsTypeRouter from './productsType.routes';
 import loginRouter from './login.routes';
 import restaurantsRouter from './restaurants.routes';
 
@@ -8,6 +9,7 @@ const router = Router();
 
 router.use('/categories', categoriesRouter);
 router.use('/products', productsRouter);
+router.use('/products-type', productsTypeRouter);
 router.use('/login', loginRouter);
 router.use('/restaurants', restaurantsRouter);
 
