a371ca8dd2786ed0aff45b1efedb82634cfba6ec
Ebio Narduche <ebionarduche77@gmail.com>
2024-01-20 11:46:34 -0300

    feat: Add HTML structure to login page

--- app/client/src/pages/Login.jsx
+++ app/client/src/pages/Login.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import mouth from '../images/mouth.png';
 
 function Login() {
     return(
-        <div>
-            <h1>Login</h1>
+        <div className='login-container'>
+            <div className='login-header'>
+                <button>voltar</button>
+                <span>Não tem conta?</span>
+                <span>Cadastre-se</span>
+            </div>
+
+            <h1>Acesse seus restaurantes prediletos</h1>
+            <img src={ mouth } alt="" />
+
+            <div className='login-form'>
+                <input type="text" placeholder="Email" />
+                <input type="password" placeholder="Senha" />
+                <button>Entrar</button>
+                <span>Esqueci minha senha</span>
+            </div>
         </div>
     );
 }
