ae021a4b959124272f852681637e18883d325b78
Ebio Narduche <ebionarduche77@gmail.com>
2024-01-21 17:57:34 -0300

    feat: HTML structure products-cards and Styles

--- app/client/src/pages/RestaurantDetails.jsx
+++ app/client/src/pages/RestaurantDetails.jsx
@@ -1,11 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import RestaurantContext from '../context/RestaurantContext';
+import axios from 'axios';
+import ProductsCard from '../components/ProductsCard';
 
 
 function RestaurantDetails() {
+    const [products, setProducts] = useState([]);
     const { restaurantsData } = useContext(RestaurantContext);
     const { id } = useParams();
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:3001/products');
+                setProducts(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchData();
+    }, []);
 
     const restaurant = restaurantsData[id - 1];
     
@@ -16,6 +32,7 @@ function RestaurantDetails() {
             <h1>{restaurant.restaurantName}</h1>
             <p>{restaurant.resume}</p>
             <p>{restaurant.address}</p>
+            <ProductsCard products={products} />
         </div>
     );
 }
