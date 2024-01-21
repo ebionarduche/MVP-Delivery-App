9040baa6c1ce717befb646134cef9c495b1877a4
Ebio Narduche <ebionarduche77@gmail.com>
2024-01-21 11:33:48 -0300

    Basic html structure RestaurantsDetails and Styles Cards settings

--- app/client/src/pages/RestaurantDetails.jsx
+++ app/client/src/pages/RestaurantDetails.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import RestaurantContext from '../context/RestaurantContext';
+
 
 function RestaurantDetails() {
-    return <div>
-        <h1>Restaurant Details</h1>
-    </div>;
+    const { restaurantsData } = useContext(RestaurantContext);
+    const { id } = useParams();
+
+    const restaurant = restaurantsData[id - 1];
+    
+    return (
+        <div>
+            <img src={restaurant.wallpaper} alt="" />
+            <img src={restaurant.logo} alt="" />
+            <h1>{restaurant.restaurantName}</h1>
+            <p>{restaurant.resume}</p>
+            <p>{restaurant.address}</p>
+        </div>
+    );
 }
 
 export default RestaurantDetails;
