b57af719cff8c324ddfcb00fb19e5fc179a7ba96
Ebio Narduche <ebionarduche77@gmail.com>
2024-01-21 13:14:32 -0300

    feat: Implementation of a restaurant search filter

--- app/client/src/components/Header.jsx
+++ app/client/src/components/Header.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import RestaurantContext from '../context/RestaurantContext';
+import axios from 'axios';
+
 
 function Header() {
+    const [searchTerm, setsearchTerm] = useState('');
+    const { setrestaurantsData, originalData } = useContext(RestaurantContext);
+
+    const handleInputChange = async ({ target: { value } }) => {
+        setsearchTerm(value);
+        try {
+            const {data} = await axios.get(`http://localhost:3001/restaurants/search?query=${value}`);
+            console.log(data);
+            setrestaurantsData(data);
+        } catch (error) {
+            console.error(error);
+        }
+        if(value === '') {
+            setrestaurantsData(originalData);
+        }
+    };
+
     return (
         <div>
             <h1>Header</h1>
+            <input 
+                id="search-bar" 
+                type="text"
+                name="search-bar"
+                value={searchTerm}
+                onChange={handleInputChange}
+            />
         </div>
     );
 }
--- app/client/src/pages/Home.jsx
+++ app/client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react';
 import RestaurantContext from '../context/RestaurantContext';
 import RestaurantCard from '../components/RestaurantCard';
+import Header from '../components/Header';
 
 
 
@@ -10,7 +11,7 @@ function Home() {
 
     return(
         <div>
-            <h1>Home</h1>
+            <Header />
             <RestaurantCard restaurantsData={restaurantsData}/>
         </div>
     );
