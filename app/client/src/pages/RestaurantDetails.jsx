17a4d9a4c11a82b74a565796bed72e555465d0b5
Ebio Narduche <ebionarduche77@gmail.com>
2024-01-22 10:12:02 -0300

    feat: Add Icons nav

--- app/client/src/pages/RestaurantDetails.jsx
+++ app/client/src/pages/RestaurantDetails.jsx
@@ -4,7 +4,7 @@ import RestaurantContext from '../context/RestaurantContext';
 import axios from 'axios';
 import ProductsCard from '../components/ProductsCard';
 import './style/RestaurantDetails.css';
-import Header from '../components/Header';
+// import Header from '../components/Header';
 
 
 function RestaurantDetails() {
@@ -29,11 +29,10 @@ function RestaurantDetails() {
     
     return (
         <div className='restarant-details-container'>
-            <Header />
             <img className='restarant-details-wallpaper' src={restaurant.wallpaper} alt="" />
-            <img className='restarant-details-logo' src={restaurant.logo} alt="" />
             <div className='restarant-details-content'>
                 <div>
+                    <img className='restarant-details-logo' src={restaurant.logo} alt="" />
                     <h1 className='restarant-details-name'>{restaurant.restaurantName}</h1>
                     <p className='restarant-details-resume'>{restaurant.resume}</p>
                 </div>
