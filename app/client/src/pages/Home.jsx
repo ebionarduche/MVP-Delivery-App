800dcb61a8f2afcf637f7903a56c3b18ce42cd74
Ebio Narduche <ebionarduche77@gmail.com>
2024-01-23 10:56:22 -0300

    feat:  Category Rendering Logic

--- app/client/src/pages/Home.jsx
+++ app/client/src/pages/Home.jsx
@@ -3,18 +3,21 @@ import RestaurantContext from '../context/RestaurantContext';
 import RestaurantCard from '../components/RestaurantCard';
 import Header from '../components/Header';
 import CategoriesList from '../components/CategoriesList';
-// import CarouselProducts from '../components/CarouselProducts';
-
+import CarouselCategories from '../components/CarouselCategories';
+import { useMediaQuery } from 'react-responsive';
 
 
 
 function Home() {
     const { restaurantsData } = useContext(RestaurantContext);
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+
 
     return(
         <div>
             <Header />
-            <CategoriesList />
+            {!isMobile && <CategoriesList />}
+            {isMobile && <CarouselCategories />}
             <RestaurantCard restaurantsData={restaurantsData}/>
         </div>
     );
