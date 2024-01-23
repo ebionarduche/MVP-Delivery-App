307c4f611f846f66a2fa154688539b41fcc4ad97
Ebio Narduche <ebionarduche77@gmail.com>
2024-01-23 14:10:45 -0300

    feat: Landing page responsive

--- app/client/src/pages/LandingPage.jsx
+++ app/client/src/pages/LandingPage.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import mouth from '../images/mouth.png';
 import './style/LandingPage.css';
+import { useMediaQuery } from 'react-responsive';
+
 
 
 function LandingPage() {
+    const isMobile = useMediaQuery({ maxWidth: 768 });
     const history = useHistory(); 
 
     const navigatePage = (link) => {
@@ -23,8 +26,14 @@ function LandingPage() {
                     </div>
                     <img className='landing-page-logo' src={mouth} alt="" />
                 </div>
-                <h2 className='landing-page-sub-title'>Falta pouco para matar sua fome!</h2>
-                <p className='landing-page-content'>Como deseja continuar ?</p>
+                {
+                    !isMobile && (
+                        <>
+                            <h2 className='landing-page-sub-title'>Falta pouco para matar sua fome!</h2>
+                            <p className='landing-page-content'>Como deseja continuar ?</p>
+                        </>
+                    )
+                }
                 <button
                     type='button'
                     className='landing-page-button-create-account'
