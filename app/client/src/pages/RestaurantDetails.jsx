6e2c4bfbff89e6640b9cebcf20f916ddf7a87934
Ebio Narduche <ebionarduche77@gmail.com>
2024-01-22 22:33:11 -0300

    refactor: Html and Css RestaurantDetails

--- app/client/src/pages/RestaurantDetails.jsx
+++ app/client/src/pages/RestaurantDetails.jsx
@@ -1,23 +1,27 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import RestaurantContext from '../context/RestaurantContext';
 import axios from 'axios';
 import ProductsCard from '../components/ProductsCard';
+import Header from '../components/Header';
 import './style/RestaurantDetails.css';
-// import Header from '../components/Header';
 
 
 function RestaurantDetails() {
-    const [products, setProducts] = useState([]);
-    const { restaurantsData } = useContext(RestaurantContext);
+    const [meals, setMeals] = useState([]);
+    const [drinks, setDrinks] = useState([]);
+    const [restaurant, setrestaurant] = useState([]);
     const { id } = useParams();
 
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('http://localhost:3001/products');
-                setProducts(response.data);
+                const responseMeals = await axios.get('http://localhost:3001/products/type/1');
+                const responseDrinks = await axios.get('http://localhost:3001/products/type/2');
+                const responserestaurant = await axios.get(`http://localhost:3001/restaurants/find/${id}`);
+                setrestaurant(responserestaurant.data);
+                setMeals(responseMeals.data);
+                setDrinks(responseDrinks.data);
             } catch (error) {
                 console.error(error);
             }
@@ -25,25 +29,30 @@ function RestaurantDetails() {
         fetchData();
     }, []);
 
-    const restaurant = restaurantsData[id - 1];
-    
     return (
-        <div className='restarant-details-container'>
-            <img className='restarant-details-wallpaper' src={restaurant.wallpaper} alt="" />
-            <div className='restarant-details-content'>
-                <div>
-                    <img className='restarant-details-logo' src={restaurant.logo} alt="" />
-                    <h1 className='restarant-details-name'>{restaurant.restaurantName}</h1>
-                    <p className='restarant-details-resume'>{restaurant.resume}</p>
-                </div>
-                <p className='restarant-details-address'>{restaurant.address}</p>
+        <div>
+            <Header />
+            <div className='restarant-details-container'>
+                <img className='restarant-details-wallpaper' src={restaurant.wallpaper} alt="" />
+                <section className='restarant-details-content'>
+                    <div className='restarant-details-logo-container'>
+                        <img className='restarant-details-logo' src={restaurant.logo} alt="" />
+                        <div>
+                            <h1 className='restarant-details-name'>{restaurant.restaurantName}</h1>
+                            <p className='restarant-details-resume'>{restaurant.resume}</p>
+                        </div>
+                    </div>
+                    <div className='restarant-details-info-container'>
+                        <span className='restarant-details-info'>{restaurant.address}</span>
+                        <p className='restarant-details-info'>{restaurant.phone}</p>
+                    </div>
+                </section>
+                <ProductsCard products={meals} />
+                <ProductsCard products={drinks} />
             </div>
-            <h2>Refeições</h2>
-            <ProductsCard products={products} />
-            <h2>Bebidas</h2>
-
         </div>
     );
+    
 }
 
 export default RestaurantDetails;
