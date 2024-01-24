c4730576646a9fc98399d328ad4a484b200222d5
Ebio Narduche <ebionarduche77@gmail.com>
2024-01-23 22:24:57 -0300

    feat: decode jwt token header rendering

--- app/client/src/components/Header.jsx
+++ app/client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import mouth from '../images/mouth.png';
 import user from '../images/icons/user.svg';
@@ -7,27 +7,40 @@ import shoppingBag from '../images/icons/shopping-bag.svg';
 import { useMediaQuery } from 'react-responsive';
 import './style/Header.css';
 import SearchBar from './SearchBar';
-
-
+import { jwtDecode } from 'jwt-decode'; 
 
 function Header() {
     const history = useHistory();
     const isMobile = useMediaQuery({ maxWidth: 768 });
+
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        const tokenString = localStorage.getItem('userData');
+        
+        if (tokenString) {
+            try {
+                const tokenObject = JSON.parse(tokenString);
+                const decodedToken = jwtDecode(tokenObject.token); 
+                setUserData(decodedToken);
+            } catch (error) {
+                console.error('Erro ao decodificar o token:', error);
+            }
+        }
+    }, []);
 
     const handleLogOut = () => {
         localStorage.clear();
         history.push('/');
     };
 
-    
     return (
         <div className='header-container'>
             {isMobile ? (
                 <section className='header-container-mobile'>
-                    
                     <div>
-                        <p>{`Olá ${'Usuário'}, você está neste endereço?`}</p>
-                        <span>{`${'Endereço indisponível'}`}</span>
+                        <p>{`Olá ${userData ? userData.userName : 'Usuário'}, você está neste endereço?`}</p>
+                        <span>{`${userData ? userData.address : 'Endereço indisponível'}`}</span>
                     </div>
                 </section>
             ) : (
@@ -35,12 +48,10 @@ function Header() {
                     <img className='header-img' src={mouth} alt="" />
                     <SearchBar />
                     <section>
-                        <p>{`Olá ${name || 'Usuário'}, você está neste endereço?`}</p>
-                        <span>{`${name || 'Endereço indisponível'}`}</span>
+                        <p>{`Olá ${userData ? userData.userName : 'Usuário'}, você está neste endereço?`}</p>
+                        <span>{`${userData ? userData.address : 'Endereço indisponível'}`}</span>
                     </section>
-                    <button
-                        className='header-button'
-                    >                    
+                    <button className='header-button'>
                         <img src={user} alt="" width='30px' />
                     </button>
 
